5868839a780b24295063f852b0de9cdbe79c84c9
Aaron Madlon-Kay <aaron@madlon-kay.com>
2018-03-13 16:51:53 +0900

    Process all dropped files

--- ImageQualityTester.sh
+++ ImageQualityTester.sh
@@ -5,9 +5,15 @@ export MAGICK_HOME="$(pwd)/ImageMagick"
 export PATH="${MAGICK_HOME}/bin:$PATH"
 export DYLD_LIBRARY_PATH="${MAGICK_HOME}/lib/"
 
-file="$1"
-base="${file%.*}"
+function processOne() {
+    file="$1"
+    base="${file%.*}"
 
-for ((q = 100; q > 0; q -= 10)); do
-    convert "$file" -quality $q "${base}_q${q}.jpg"
+    for ((q = 100; q > 0; q -= 10)); do
+        convert "$file" -quality $q "${base}_q${q}.jpg"
+    done
+}
+
+for arg in "$@"; do
+    processOne "$arg"
 done
